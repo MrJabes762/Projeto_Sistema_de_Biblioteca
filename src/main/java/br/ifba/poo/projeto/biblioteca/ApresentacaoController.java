61232585346faf18bff882151d691d9742674774
Jabes Cajazeira <118413268+MrJabes762@users.noreply.github.com>
2023-05-31 02:28:56 -0300

    Desenvolvimento (#6)

---
+++ src/main/java/br/ifba/poo/projeto/biblioteca/ApresentacaoController.java
@@ -0,0 +1,16 @@
+
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.scene.control.Button;
+
+public class ApresentacaoController {
+
+    @FXML
+    private Button botaodeentar;
+
+    @FXML
+    void Entrar(ActionEvent event) {
+        
+    }
+
+}
