7a551d90efb4f31cfea0491a1aa6b28bc7806340
MrJabes762 <jabessilva31@gmail.com>
2023-05-31 02:28:03 -0300

    Alguns acertos

--- src/main/java/module-info.java
+++ src/main/java/module-info.java
@@ -1,6 +1,7 @@
 module br.ifba.poo.projeto.biblioteca {
     requires javafx.controls;
     requires javafx.fxml;
+    requires javafx.graphics;
 
     opens br.ifba.poo.projeto.biblioteca to javafx.fxml;
     exports br.ifba.poo.projeto.biblioteca;
