d1e808b5d22048faf6728087c3e9b8aa2394dc99
MrJabes762 <jabessilva31@gmail.com>
2023-05-31 00:20:37 -0300

    Configuração do Ambiente

---
+++ projetobibliotecajavafx/src/main/java/br/ifba/poo/projeto/biblioteca/SecondaryController.java
@@ -0,0 +1,12 @@
+package br.ifba.poo.projeto.biblioteca;
+
+import java.io.IOException;
+import javafx.fxml.FXML;
+
+public class SecondaryController {
+
+    @FXML
+    private void switchToPrimary() throws IOException {
+        App.setRoot("primary");
+    }
+}
